70687c5219386cf17b9a497458e6566df0bdbc35
Sérgio Santos <tavinhomelo69@gmail.com>
2025-05-19 15:07:40 -0300

    Initial commit

---
+++ JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btn21 = new Button();
+            btn31 = new Button();
+            btn32 = new Button();
+            btn33 = new Button();
+            pictureBox2 = new PictureBox();
+            btn22 = new Button();
+            btn12 = new Button();
+            btn23 = new Button();
+            btn13 = new Button();
+            btn11 = new Button();
+            pictureBox1 = new PictureBox();
+            pibFechar = new PictureBox();
+            PibReiniciar = new PictureBox();
+            lblinfo = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pibFechar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PibReiniciar).BeginInit();
+            SuspendLayout();
+            // 
+            // btn21
+            // 
+            btn21.FlatStyle = FlatStyle.Flat;
+            btn21.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn21.Location = new Point(55, 292);
+            btn21.Name = "btn21";
+            btn21.Size = new Size(92, 83);
+            btn21.TabIndex = 3;
+            btn21.UseVisualStyleBackColor = true;
+            // 
+            // btn31
+            // 
+            btn31.FlatStyle = FlatStyle.Flat;
+            btn31.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn31.Location = new Point(55, 414);
+            btn31.Name = "btn31";
+            btn31.Size = new Size(92, 83);
+            btn31.TabIndex = 5;
+            btn31.UseVisualStyleBackColor = true;
+            // 
+            // btn32
+            // 
+            btn32.FlatStyle = FlatStyle.Flat;
+            btn32.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn32.Location = new Point(175, 414);
+            btn32.Name = "btn32";
+            btn32.Size = new Size(92, 83);
+            btn32.TabIndex = 6;
+            btn32.UseVisualStyleBackColor = true;
+            // 
+            // btn33
+            // 
+            btn33.FlatStyle = FlatStyle.Flat;
+            btn33.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn33.Location = new Point(298, 414);
+            btn33.Name = "btn33";
+            btn33.Size = new Size(92, 83);
+            btn33.TabIndex = 7;
+            btn33.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(12, 120);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(424, 410);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // btn22
+            // 
+            btn22.FlatStyle = FlatStyle.Flat;
+            btn22.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn22.Location = new Point(175, 292);
+            btn22.Name = "btn22";
+            btn22.Size = new Size(92, 83);
+            btn22.TabIndex = 4;
+            btn22.UseVisualStyleBackColor = true;
+            btn22.Click += button3_Click;
+            // 
+            // btn12
+            // 
+            btn12.FlatStyle = FlatStyle.Flat;
+            btn12.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn12.Location = new Point(175, 165);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(92, 83);
+            btn12.TabIndex = 1;
+            btn12.UseVisualStyleBackColor = true;
+            // 
+            // btn23
+            // 
+            btn23.FlatStyle = FlatStyle.Flat;
+            btn23.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn23.Location = new Point(298, 292);
+            btn23.Name = "btn23";
+            btn23.Size = new Size(92, 83);
+            btn23.TabIndex = 4;
+            btn23.UseVisualStyleBackColor = true;
+            // 
+            // btn13
+            // 
+            btn13.FlatStyle = FlatStyle.Flat;
+            btn13.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn13.Location = new Point(298, 165);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(92, 83);
+            btn13.TabIndex = 2;
+            btn13.UseVisualStyleBackColor = true;
+            // 
+            // btn11
+            // 
+            btn11.FlatStyle = FlatStyle.Flat;
+            btn11.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn11.Location = new Point(55, 165);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(92, 83);
+            btn11.TabIndex = 0;
+            btn11.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(424, 102);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // pibFechar
+            // 
+            pibFechar.Image = (Image)resources.GetObject("pibFechar.Image");
+            pibFechar.Location = new Point(402, 3);
+            pibFechar.Name = "pibFechar";
+            pibFechar.Size = new Size(68, 50);
+            pibFechar.SizeMode = PictureBoxSizeMode.Zoom;
+            pibFechar.TabIndex = 10;
+            pibFechar.TabStop = false;
+            // 
+            // PibReiniciar
+            // 
+            PibReiniciar.Image = (Image)resources.GetObject("PibReiniciar.Image");
+            PibReiniciar.Location = new Point(0, 3);
+            PibReiniciar.Name = "PibReiniciar";
+            PibReiniciar.Size = new Size(57, 50);
+            PibReiniciar.SizeMode = PictureBoxSizeMode.Zoom;
+            PibReiniciar.TabIndex = 11;
+            PibReiniciar.TabStop = false;
+            // 
+            // lblinfo
+            // 
+            lblinfo.AutoSize = true;
+            lblinfo.Font = new Font("Comic Sans MS", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblinfo.Location = new Point(12, 535);
+            lblinfo.Name = "lblinfo";
+            lblinfo.Size = new Size(286, 67);
+            lblinfo.TabIndex = 12;
+            lblinfo.Text = "É vez do X";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(481, 611);
+            Controls.Add(lblinfo);
+            Controls.Add(PibReiniciar);
+            Controls.Add(pibFechar);
+            Controls.Add(pictureBox1);
+            Controls.Add(btn33);
+            Controls.Add(btn32);
+            Controls.Add(btn31);
+            Controls.Add(btn22);
+            Controls.Add(btn23);
+            Controls.Add(btn21);
+            Controls.Add(btn13);
+            Controls.Add(btn12);
+            Controls.Add(btn11);
+            Controls.Add(pictureBox2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pibFechar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PibReiniciar).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button btn21;
+        private Button btn31;
+        private Button btn32;
+        private Button btn33;
+        private PictureBox pictureBox2;
+        private Button btn22;
+        private Button btn12;
+        private Button btn23;
+        private Button btn13;
+        private Button btn11;
+        private PictureBox pictureBox1;
+        private PictureBox pibFechar;
+        private PictureBox PibReiniciar;
+        private Label lblinfo;
+    }
+}
